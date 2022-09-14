da867086ad55910affdbd8b174c6191058d453fd
Ken Schwartz <ken.schwartz@beam.dental>
2022-09-14 13:21:14 -0400

    initial page structure

---
+++ pages/wifiProblems/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '/styles/Home.module.css'
+
+const WifiProblems: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>WifiProblems</h1>
+    </div>
+  )
+}
+
+export default WifiProblems
