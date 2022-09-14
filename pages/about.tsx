70a4f7df33dbe738b2ff9353d14f2019a061010b
John Christopher Jones <john.jones@beam.dental>
2022-09-14 17:51:47 -0400

    feat: fix the link mouseover behavior and tidy up the page layout

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,24 +3,29 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import peBlueSmall from "../public/pe-blue-small.png";
+import peGreenSmall from "../public/pe-green-small.png";
+import pePinkSmall from "../public/pe-pink-small.png";
 import styles from "../styles/About.module.css";
-import * as TeamMembers from "./api/teammembers";
+import type { Payload, TeamMember, TeamMemberLink } from "./api/teammembers";
 
-const gravatarUrl = (links: TeamMembers.TeamMemberLink[]): string => {
+const gravatarUrl = (links: TeamMemberLink[]): string => {
   const link = links.find((link) => link.name === "gravatar");
   return link!.url;
 };
 
-const TeamMemberLink = ({ displayName, name, url }: TeamMembers.TeamMemberLink) => (
-  <div className={styles.card} key={name}>
-    <Link href={url}>{displayName}</Link>
-  </div>
+const TeamMemberLink = ({ displayName, name, url }: TeamMemberLink) => (
+  <Link href={url}>
+    <a className={styles.card} key={name}>
+      {displayName}
+    </a>
+  </Link>
 );
 
-const TeamMember = ({ links, name }: TeamMembers.TeamMember) => (
-  <div className={styles.cardgrid}>
+const TeamMember = ({ links, name }: TeamMember) => (
+  <div className={styles.membergrid}>
     <Image src={gravatarUrl(links)} width="50px" height="50px" alt={name} />
-    <h4>{name}</h4>
+    <h3>{name}</h3>
     <div>
       {links.map((memberLink) => (
         <TeamMemberLink key={memberLink.name} {...memberLink}></TeamMemberLink>
@@ -30,13 +35,12 @@ const TeamMember = ({ links, name }: TeamMembers.TeamMember) => (
 );
 
 const About: NextPage = () => {
-
-  const [members, setMembers] = useState<TeamMembers.Payload>({ data: [] });
+  const [members, setMembers] = useState<Payload>({ data: [] });
 
   useEffect(() => {
     const fetchMembers = async () => {
       const response = await fetch("/api/teammembers");
-      return (await response.json()) as TeamMembers.Payload;
+      return (await response.json()) as Payload;
     };
 
     fetchMembers().then(setMembers).catch(console.error);
@@ -47,17 +51,37 @@ const About: NextPage = () => {
       <Head>
         <title>Party Enamels - About</title>
       </Head>
-      <div>
-        <h1>About Us</h1>
-      </div>
-      <main>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Meet the Party Enamels!</h1>
+
+        <p className={styles.description}>
+          <Image src={pePinkSmall} alt="Party Enamels Pink" />
+          <Image src={peGreenSmall} alt="Party Enamels Green" />
+          <Image src={peBlueSmall} alt="Party Enamels Blue" />
+        </p>
+
         <h2>Members</h2>
-        <div className={`${styles.cardlayout}`}>
+        <div className={`${styles.grid}`}>
           {members.data.map((member) => (
             <TeamMember key={member.name} {...member}></TeamMember>
           ))}
         </div>
       </main>
+
+      <footer className={styles.footer}>
+        <a>
+          Powered by{" "}
+          <span className={styles.logo}>
+            <Image
+              src={peGreenSmall}
+              alt="Party Enamels Green"
+              width={42}
+              height={36}
+            />
+          </span>
+        </a>
+      </footer>
     </div>
   );
 };
