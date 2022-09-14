bff8c1fff8f0ccb3310c4594f4c8fb6c54fc79e4
Ken Schwartz <ken.schwartz@beam.dental>
2022-09-14 14:17:02 -0400

    added soundcloud player

--- pages/wifiProblems/index.tsx
+++ pages/wifiProblems/index.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from 'next'
-import styles from '/styles/Home.module.css'
 
 const WifiProblems: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <h1>WifiProblems</h1>
+
+      <iframe width="100%" height="115" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/307025543&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
     </div>
   )
 }
 
 export default WifiProblems
+
+// Carousel
+// Image upload
+// Music
